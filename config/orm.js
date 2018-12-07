d11bf1910374eb11e0eabd5338bb3078437b3077
Alexander <alexstockton09@gmail.com>
2018-12-06 20:11:11 -0600

    working on the orm functions

--- config/orm.js
+++ config/orm.js
@@ -4,9 +4,15 @@ const connection = require("../config/connection.js");
 //create the object for the sql statement functions
 var orm = {
     //selectAll() function
-
+    selectAll: function(tableInput) {
+        var queryString = "SELECT * FROM ??;";
+        connection.query(queryString, [tableInput], function(err, result) {
+            if(err) throw err;
+            console.log(result);
+        });
+    },
     //insertOne() function to create a burger
-
+    insertOne: function
     //updateOne() 
 
 }
